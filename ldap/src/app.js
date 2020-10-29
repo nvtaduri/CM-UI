faab60106e596cc88f6f64da1e5e195c5699ad34
unknown <taduri.nagarjunavihari@gmail.com>
2020-10-29 23:59:09 +0530

    ldap authentication integrated

--- ldap/src/app.js
+++ ldap/src/app.js
@@ -1,52 +1,47 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+let ldapService = require("./services/ldap.service");
 
 app.use(bodyParser.json());
-let ActiveDirectory = require('activedirectory');
-let config = {
-  url: 'ldap://192.168.3.99:389',
-  baseDN: 'dc=sgpltech,dc=com',
-  attributes: {
-    user: ['*'],
-    group: ['*']
-  }
-}
-let ad = new ActiveDirectory(config);
 
 app.get("/", (req, res) => {
-  let username = 'aduser1';
-  let password = 'Soft@AD001';
+  let username = "aduser1";
+  let password = "Soft@AD001";
   ad.authenticate(username, password, function (err, auth) {
     if (err) {
-      console.log('ERROR: ' + JSON.stringify(err));
+      console.log("ERROR: " + JSON.stringify(err));
       return;
     }
     if (auth) {
-      console.log('Authenticated!');
+      console.log("Authenticated!");
       res.json({ msg: "ldap service" });
-    }
-    else {
-      console.log('Authentication failed!');
+    } else {
+      console.log("Authentication failed!");
     }
   });
 });
 
 app.post("/api/v1/ldap/authenticate", async (req, res) => {
-  ad.authenticate(req.body.username, req.body.password, function (err, auth) {
-    if (err) {
-      console.log('ERROR: ' + JSON.stringify(err));
-      return;
-    }
-    if (auth) {
-      console.log('Authenticated!');
-      res.json({ success: true, msg: "User Authenticated Successfully" });
-    }
-    else {
-      console.log('Authentication failed!');
-      res.json({ success: false, msg: JSON.stringify(err) });
-    }
-  });
+  ldapService
+    .authenticateUser(req.body.username, req.body.password)
+    .then((response) => {
+      res.send(user);
+    });
+  // ad.authenticate(req.body.username, req.body.password, function (err, auth) {
+  //   if (err) {
+  //     console.log('ERROR: ' + JSON.stringify(err));
+  //     return;
+  //   }
+  //   if (auth) {
+  //     console.log('Authenticated!');
+  //     res.json({ success: true, msg: "User Authenticated Successfully" });
+  //   }
+  //   else {
+  //     console.log('Authentication failed!');
+  //     res.json({ success: false, msg: JSON.stringify(err) });
+  //   }
+  // });
 });
 
 module.exports = app;
--- ldap/src/config/index.js
+++ ldap/src/config/index.js
@@ -4,6 +4,15 @@ if (process.env.MONGO_DB_URI) {
   DB_URI = process.env.MONGO_DB_URI;
 }
 
+let AD_CONFIG = {
+  url: 'ldap://192.168.3.99:389',
+  baseDN: 'dc=sgpltech,dc=com',
+  attributes: {
+    user: ['*'],
+    group: ['*']
+  }
+}
+
 module.exports = {
-  DB_URI
+  DB_URI, AD_CONFIG
 };
